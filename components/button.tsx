ac1280fa369024e38bf936a4817af1bc405d73c6
Th1nkK1D <witheep@gmail.com>
2021-07-13 14:25:38 +0700

    Add button component

---
+++ components/button.tsx
@@ -0,0 +1,25 @@
+import { FunctionComponent, MouseEventHandler } from 'react';
+
+type ButtonProps = {
+  state: 'solid' | 'outline';
+  className?: string;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+};
+
+const Button: FunctionComponent<ButtonProps> = ({
+  children,
+  state,
+  className = '',
+  onClick,
+}) => (
+  <button
+    onClick={onClick}
+    className={`${className} border-black border-[3px] rounded-lg p-2 text-center ${
+      state === 'solid' ? 'bg-black text-white' : ''
+    }`}
+  >
+    {children}
+  </button>
+);
+
+export default Button;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Button from '../components/button';
 
 const Home = () => (
   <div>
@@ -8,21 +9,33 @@ const Home = () => (
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
-    <main className="flex flex-col justify-center items-center min-h-screen">
+    <main className="flex flex-col justify-center items-center min-h-screen space-y-8">
       <img
         src={require('../assets/images/next.png')}
         alt="Next"
-        className="w-48 mb-8"
+        className="w-48"
       />
-      <p className="text-huge">text-huge</p>
-      <p className="text-large-1">text-large-1</p>
-      <p className="text-large-2">text-large-2</p>
-      <p className="text-headline-1">text-headline-1</p>
-      <p className="text-headline-2">text-headline-2</p>
-      <p>text-body-1 (default)</p>
-      <p className="text-body-2">text-body-2</p>
-      <p className="text-small-1">text-small-1</p>
-      <p className="text-small-2">text-small-2</p>
+
+      <div className="text-center">
+        <p className="text-huge">text-huge</p>
+        <p className="text-large-1">text-large-1</p>
+        <p className="text-large-2">text-large-2</p>
+        <p className="text-headline-1">text-headline-1</p>
+        <p className="text-headline-2">text-headline-2</p>
+        <p>text-body-1 (default)</p>
+        <p className="text-body-2">text-body-2</p>
+        <p className="text-small-1">text-small-1</p>
+        <p className="text-small-2">text-small-2</p>
+      </div>
+
+      <div className="flex flex-row space-x-2">
+        <Button state="solid" onClick={() => alert('solid clicked')}>
+          solid
+        </Button>
+        <Button state="outline" onClick={() => alert('outline clicked')}>
+          outline
+        </Button>
+      </div>
     </main>
   </div>
 );
