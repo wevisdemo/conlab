119e87f3ab3958aba82cab270cacb41b3d25c867
Th1nkK1D <witheep@gmail.com>
2021-07-19 16:59:11 +0700

    Correct result summary markdown style

--- components/result/result-summery.tsx
+++ components/result/result-summery.tsx
@@ -1,5 +1,9 @@
-import { FunctionComponent } from 'react';
+import { CSSProperties, FunctionComponent } from 'react';
 import ReactMarkdown from 'react-markdown';
+import {
+  NormalComponents,
+  SpecialComponents,
+} from 'react-markdown/src/ast-to-react';
 import { Result, ResultOption } from '../../data/topics';
 import Card from '../card';
 import Pill from '../pill';
@@ -41,22 +45,47 @@ const ResultSummary: FunctionComponent<ResultSummaryProps> = ({
   };
 
   return (
-    <Card className="flex flex-col items-center space-y-2">
+    <Card className="flex flex-col items-center">
       <h3 className="text-body-2 font-black">ข้อเสนอรัฐธรรมนูญในฝัน</h3>
-      <Pill className="text-small-1">
+      <Pill className="text-small-1 mt-4 mb-6">
         หมวดที่ {topicNumber}: {shortTitle}
       </Pill>
-      <div>
-        <ReactMarkdown
-          components={{
-            code: renderDropdown,
-          }}
-        >
-          {resultTextMarkdown}
-        </ReactMarkdown>
-      </div>
+
+      <ReactMarkdown
+        skipHtml
+        className="test-left space-y-4"
+        components={{
+          code: renderDropdown,
+          ...markdownComponents,
+        }}
+      >
+        {
+          resultTextMarkdown.replaceAll?.(
+            '        ',
+            '  '
+          ) /* Stackedit indent with 8 spaces */
+        }
+      </ReactMarkdown>
     </Card>
   );
+};
+
+const getIndentedListStyle = (depth: number): CSSProperties => ({
+  marginLeft: depth > 0 ? depth * 20 : 25,
+});
+
+const markdownComponents: Partial<NormalComponents & SpecialComponents> = {
+  ul: ({ children, depth }) => (
+    <ul style={getIndentedListStyle(depth)}>{children}</ul>
+  ),
+  ol: ({ children, depth }) => (
+    <ol style={getIndentedListStyle(depth)}>{children}</ol>
+  ),
+  li: ({ children, ordered }) => (
+    <li className={`my-1 ${ordered ? 'list-decimal' : 'list-disc'}`}>
+      {children}
+    </li>
+  ),
 };
 
 export default ResultSummary;
