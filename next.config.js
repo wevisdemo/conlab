5f411a1e50ca551303cb828eba208fb5ee2cb534
Narat S <me@mixth.net>
2021-07-22 01:52:21 +0700

    Workaround for next/image bug awaiting to be released

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ module.exports = withOptimizedImages({
   basePath: process.env.BASE_PATH || '',
   reactStrictMode: true,
   images: {
+    loader: 'imgix', // hack: https://github.com/vercel/next.js/issues/21079
+    path: '',
     disableStaticImages: true,
   },
   inlineImageLimit: -1,
