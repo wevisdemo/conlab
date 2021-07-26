c7e7c96d39fc119fb226fa2b934c226625788f1b
mixth <me@mixth.net>
2021-07-26 16:16:16 +0700

    Create item page

--- pages/topics/[topicNumber]/index.tsx
+++ pages/topics/[topicNumber]/index.tsx
@@ -1,17 +1,76 @@
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { FunctionComponent } from 'react';
+import Button from '../../../components/button';
+import Footer from '../../../components/footer';
+import HowItWork from '../../../components/how-it-work';
 import NavigationBar from '../../../components/navigation-bar';
+import Pill from '../../../components/pill';
+import Markdown from '../../../components/result/markdown';
+import TopicCard from '../../../components/topic-card';
 import {
   getTopicsStaticPaths,
   getTopicsStaticProps,
   TopicPageProps,
 } from '../../../utils/topics-route';
 
-const Topic: FunctionComponent<TopicPageProps> = () => (
-  <div>
-    <NavigationBar theme="transparent" />
-    <div>รัฐสภาไทย...ควรเป็นแบบไหน</div>
-  </div>
-);
+const Topic: FunctionComponent<TopicPageProps> = ({ topic }) => {
+  const router = useRouter();
+  const goToQuiz = () => {
+    router.push(`/topics/${topic.topicNumber}/quiz`);
+  };
+
+  return (
+    <div>
+      <Head>
+        <title>{topic.shortTitle} | Dream Constitution</title>
+      </Head>
+      <NavigationBar className="bg-blue-300" theme="transparent" />
+      <main className="bg-gray-100">
+        <div className="section bg-blue-300 space-y-4">
+          <Pill>
+            หมวดที่ {topic.topicNumber}: {topic.shortTitle}
+          </Pill>
+          <div className="text-large-2">{topic.title}</div>
+          <Markdown>{topic.descriptionMarkdown}</Markdown>
+          <div>
+            <img
+              src={topic.iconUrl}
+              alt=""
+              width="120"
+              height="120"
+              className="my-5"
+            ></img>
+          </div>
+          <Button onClick={goToQuiz} state="solid" className="w-full">
+            เริ่มกันเลย
+          </Button>
+          <Button state="outline" className="w-full">
+            นี่คืออะไร
+          </Button>
+        </div>
+        <div className="section mt-8">
+          <HowItWork />
+        </div>
+        <div className="section mt-2 mb-12">
+          <div className="text-headline-2 mb-10 w-full">
+            ถ้าพร้อมแล้วก็เริ่มเลย...
+          </div>
+          <TopicCard
+            topicNumber={topic.topicNumber}
+            shortTitle={topic.shortTitle}
+            title={topic.title}
+            iconUrl={topic.iconUrl}
+            className="bg-blue-300"
+            onClick={goToQuiz}
+            showButton={true}
+          />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
 export default Topic;
 
