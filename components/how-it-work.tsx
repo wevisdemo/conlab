wevisdemo/conlab

commit 5675f08fb9d549058ea134722ac477aabc93e004

Use default spacing scale instead of specific pixel

components/how-it-work.tsx
@@ -5,7 +5,7 @@ type HowItWorkProps = {
 };
 
 const HowItWork: FunctionComponent<HowItWorkProps> = ({ className }) => (
-  <div className={`${className} flex flex-col items-center space-y-[16px]`}>
+  <div className={`${className} flex flex-col items-center space-y-4`}>
     <h1 className="text-headline-1">
       Dream Consitution จะช่วยคุณออกแบบรัฐธรรมนูญได้อย่างไร?
     </h1>
@@ -43,11 +43,11 @@ const HowItWorkItem: FunctionComponent<HowItWorkItemProps> = ({
   orderNo,
   text,
 }) => (
-  <div className="bg-gray-0 flex items-center space-x-[8px] p-[8px] rounded-[12px] w-[100%]">
+  <div className="bg-gray-0 flex items-center space-x-2 p-2 rounded-3 w-full">
     <img
       src={require(`../assets/images/${imageName}`)}
       alt=""
-      className="w-[80px] h-[80px]"
+      className="w-10 h-10"
     />
     <div className="flex-grow-1">
       <div className="font-bold">{orderNo}</div>

components/navigation-bar.tsx
@@ -17,7 +17,7 @@ const NavigationBar: FunctionComponent<NavigationBarProps> = ({
     <div
       className={`${className} ${
         theme === 'black' ? 'bg-black' : undefined
-      } w-[100%]`}
+      } w-full`}
     >
       <TopBar
         theme={theme}
@@ -42,9 +42,9 @@ const TopBar: FunctionComponent<TopBarProps> = ({
   className,
   onClick,
 }) => (
-  <div className={`${className} w-[100%] flex justify-between items-start`}>
+  <div className={`${className} w-full flex justify-between items-start`}>
     <img
-      className={theme === 'transparent' ? 'p-[16px]' : 'px-[16px] py-[8px]'}
+      className={theme === 'transparent' ? 'p-4' : 'px-4 py-2'}
       src={
         theme === 'transparent'
           ? require('../assets/images/logo-large.svg')
@@ -54,7 +54,7 @@ const TopBar: FunctionComponent<TopBarProps> = ({
     ></img>
     <button onClick={onClick}>
       <img
-        className="my-[26px] mx-[24px]"
+        className="my-6.5 mx-6"
         src={
           button === 'close'
             ? require('../assets/images/close.svg')
@@ -77,9 +77,9 @@ type MenuProps = {
 const Menu: FunctionComponent<MenuProps> = ({ onClick }) => {
   const { asPath } = useRouter();
   return (
-    <div className="bg-black fixed w-[100%] h-[100%] top-0 flex flex-col">
+    <div className="bg-black fixed w-full h-full top-0 flex flex-col">
       <TopBar theme="black" button="close" onClick={onClick}></TopBar>
-      <div className="flex flex-col space-y-[16px] p-[16px] overflow-y-scroll items-center">
+      <div className="flex flex-col space-y-4 p-4 overflow-y-scroll items-center">
         <Link href="/" passHref>
           <MenuItem
             topicNumber={0}
@@ -118,15 +118,15 @@ const MenuItem: FunctionComponent<MenuItemProps> = ({
   active,
   href,
 }) => (
-  <a className="max-w-[544px] w-[100%]" href={href}>
+  <a className="max-w-[544px] w-full" href={href}>
     <div
-      className={`flex px-[8px] py-[20px] items-center space-x-[16px] ${
+      className={`flex px-2 py-5 items-center space-x-4 ${
         active
           ? 'text-black border-blue-300 bg-blue-300'
           : 'text-white border-gray-500 bg-black'
-      } border-[4px]  rounded-[12px]`}
+      } border-1  rounded-3`}
     >
-      <div className="text-large-1 min-w-[60px] text-center">{topicNumber}</div>
+      <div className="text-large-1 min-w-15 text-center">{topicNumber}</div>
       <div>
         <div className="text-small-1">{subtitle}</div>
         <div className="text-headline-2">{title}</div>

components/topic-card.tsx
@@ -24,7 +24,7 @@ const TopicCard: FunctionComponent<TopicCardProps> = ({
   <div
     className={`${className} ${
       !showButton ? 'cursor-pointer' : null
-    } border-black border-[3px] rounded-xl oblique-shadow flex flex-col items-center p-[20px] space-y-[16px] w-[100%]`}
+    } border-black border-[3px] rounded-xl oblique-shadow flex flex-col items-center p-5 space-y-4 w-full`}
     onClick={(e) => (!showButton && onClick ? onClick(e) : null)}
   >
     <Pill className="text-center">

commit ef952e23e1bfda7c88c2cc2837564b97b59993c6

Fix UI errors caused by 5675f08fb9d549058ea134722ac477aabc93e004

components/how-it-work.tsx
@@ -43,11 +43,12 @@ const HowItWorkItem: FunctionComponent<HowItWorkItemProps> = ({
   orderNo,
   text,
 }) => (
-  <div className="bg-gray-0 flex items-center space-x-2 p-2 rounded-3 w-full">
+  <div className="bg-gray-0 flex items-center space-x-2 p-2 rounded-[12px] w-full">
     <img
       src={require(`../assets/images/${imageName}`)}
       alt=""
-      className="w-10 h-10"
+      width="80"
+      height="80"
     />
     <div className="flex-grow-1">
       <div className="font-bold">{orderNo}</div>

components/navigation-bar.tsx
@@ -124,9 +124,9 @@ const MenuItem: FunctionComponent<MenuItemProps> = ({
         active
           ? 'text-black border-blue-300 bg-blue-300'
           : 'text-white border-gray-500 bg-black'
-      } border-1  rounded-3`}
+      } border-4  rounded-[12px]`}
     >
-      <div className="text-large-1 min-w-15 text-center">{topicNumber}</div>
+      <div className="text-large-1 min-w-[60px] text-center">{topicNumber}</div>
       <div>
         <div className="text-small-1">{subtitle}</div>
         <div className="text-headline-2">{title}</div>