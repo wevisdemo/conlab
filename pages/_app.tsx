029c5ce2bdb438a84706e17d42427f3c8b7375e4
Th1nkK1D <witheep@gmail.com>
2021-07-27 19:39:28 +0300

    Add smoothscroll polyfill

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
 import 'intersection-observer';
 import type { AppProps } from 'next/app';
+import smoothscroll from 'smoothscroll-polyfill';
+import { useEffect } from 'react';
 import '../assets/styles/globals.css';
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+const App = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => smoothscroll.polyfill());
+
+  return <Component {...pageProps} />;
+};
 
 export default App;
