3de03fa291d95022ad4926d2868071f7d7383803
mixth <me@mixth.net>
2021-07-29 22:39:15 +0700

    Attempt to fixed #20 by adding both basePath and assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withOptimizedImages = require('next-optimized-images');
 
 module.exports = withOptimizedImages({
+  basePath: process.env.BASE_PATH || '',
   assetPrefix: process.env.BASE_PATH || '',
   reactStrictMode: true,
   images: {
