fd5ab41d7845bf03f8260a17cfa7e3859dac5ea8
Th1nkK1D <witheep@gmail.com>
2021-07-30 11:02:33 +0300

    Fix possible duplicated key in suggested options

--- components/result/suggested-options.tsx
+++ components/result/suggested-options.tsx
@@ -12,8 +12,8 @@ const SuggestedOptions: FunctionComponent<SuggestedOptionsType> = ({
   <Card className="text-body-2 space-y-4 bg-blue-50">
     <h3 className="font-black text-center">ผลการออกแบบเบื้องต้น</h3>
     <ol className="list-decimal ml-4">
-      {options.map(({ title }) => (
-        <li key={title}>{title}</li>
+      {options.map(({ title }, index) => (
+        <li key={index}>{title}</li>
       ))}
     </ol>
   </Card>
